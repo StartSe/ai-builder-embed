d8e9ae903e1d175e806f71c31ca3556b6be239e4
arthurhrc <arthurhrc@gmail.com>
2023-10-17 11:33:14 -0300

    feat: added button

---
+++ dist/components/icons/StarIcon.d.ts
@@ -0,0 +1,2 @@
+export declare const StarIcon: () => import("solid-js").JSX.Element;
+//# sourceMappingURL=StarIcon.d.ts.map
--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,4 +1,5 @@
-import { onCleanup, onMount } from 'solid-js';
+import { StarIcon } from '@/components/icons/StarIcon'
+import { onCleanup, onMount } from 'solid-js'
 
 type Props = {
   botContainer: HTMLDivElement | undefined;
@@ -36,56 +37,59 @@ export const Badge = (props: Props) => {
     if (observer) observer.disconnect();
   });
 
- return (
-   <span
-     style={{
-       "font-size": "13px",
-       position: "absolute",
-       bottom: 0,
-       padding: "10px",
-       margin: "auto",
-       width: "100%",
-       "text-align": "center",
-       color: props.poweredByTextColor ?? defaultTextColor,
-       "background-color": props.badgeBackgroundColor ?? "#ffffff",
-       display: "flex",
-       "justify-content": "space-around",
-     }}
-   >
-     <span>
-       Powered by
-       <a
-         ref={liteBadge}
-         href={"https://www.startse.com/"}
-         target="_blank"
-         rel="noopener noreferrer"
-         class="lite-badge"
-         id="lite-badge"
-         style={{
-           "font-weight": "bold",
-           color: props.poweredByTextColor ?? defaultTextColor,
-         }}
-       >
-         <span> StartSe</span>
-       </a>
-     </span>
+return (
+  <span
+    style={{
+      "font-size": "13px",
+      position: "absolute",
+      bottom: 0,
+      padding: "10px 12px",
+      margin: "auto",
+      width: "100%",
+      "text-align": "center",
+      color: props.poweredByTextColor ?? defaultTextColor,
+      "background-color": props.badgeBackgroundColor ?? "#ffffff",
+      display: "flex",
+      "justify-content": "space-between",
+    }}
+  >
+    <span style={{ display: "flex", "align-items": "center" }}>
+      Powered by
+      <a
+        href={"https://www.startse.com/"}
+        target="_blank"
+        rel="noopener noreferrer"
+        class="lite-badge"
+        id="lite-badge"
+        style={{
+          "font-weight": "bold",
+          color: props.poweredByTextColor ?? defaultTextColor,
+          "margin-right": "3px",
+        }}
+      >
+        <span>StartSe</span>
+      </a>
+    </span>
 
-     <a
-       ref={liteBadge}
-       href={'https://forms.gle/KYeCx3PWUpozpMjQ9'}
-       target="_blank"
-       rel="noopener noreferrer"
-       class="lite-badge"
-       id="lite-badge"
-       style={{
-         "font-weight": 'bold',
-         color: props.poweredByTextColor ?? defaultTextColor,
-       }}
-     >
-       <span>Avaliar</span>
-     </a>
-   </span>
- );
+    <button
+      onClick={() => {
+        window.open("https://forms.gle/KYeCx3PWUpozpMjQ9", "_blank");
+      }}
+      style={{
+        border: "2px solid #303235",
+        "border-radius": "5px",
+        padding: "12.5px",
+        display: "flex",
+        "align-items": "center",
+        cursor: "pointer",
+      }}
+    >
+      <StarIcon />
+      <span
+        style={{ "margin-left": "5px" }}>Avaliar</span>
+    </button>
+  </span>
+);
 
+};
 
-}
