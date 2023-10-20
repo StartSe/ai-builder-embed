5f43fa78340739eecd5644b07c03461a8f2347c7
arthurhrc <arthurhrc@gmail.com>
2023-10-20 14:43:07 -0300

    fix: standardization

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,49 +1,49 @@
-import { StarIcon } from "@/components/icons/StarIcon";
-import { onCleanup, onMount } from "solid-js";
+import { StarIcon } from '@/components/icons/StarIcon'
+import { onCleanup, onMount } from 'solid-js'
 
 type Props = {
-  botContainer: HTMLDivElement | undefined;
-  poweredByTextColor?: string;
-  badgeBackgroundColor?: string;
-  showButton?: boolean;
-  buttonText?: string;
-  buttonColor?: string;
-  buttonLink?: string;
-};
+  botContainer: HTMLDivElement | undefined
+  poweredByTextColor?: string
+  badgeBackgroundColor?: string
+  showButton?: boolean
+  buttonText?: string
+  buttonColor?: string
+  buttonLink?: string
+}
 
-const defaultTextColor = "#303235";
+const defaultTextColor = '#303235'
 
 export const Badge = (props: Props) => {
-  let liteBadge: HTMLAnchorElement | undefined;
-  let observer: MutationObserver | undefined;
+  let liteBadge: HTMLAnchorElement | undefined
+  let observer: MutationObserver | undefined
 
   const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
     mutations.forEach((mutation) => {
       mutation.removedNodes.forEach((removedNode) => {
         if (
-          "id" in removedNode &&
+          'id' in removedNode &&
           liteBadge &&
-          removedNode.id == "lite-badge"
+          removedNode.id == 'lite-badge'
         ) {
-          console.log("Sorry, you can't remove the brand 😅");
-          props.botContainer?.append(liteBadge);
+          console.log("Sorry, you can't remove the brand 😅")
+          props.botContainer?.append(liteBadge)
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
   onMount(() => {
-    if (!document || !props.botContainer) return;
-    observer = new MutationObserver(appendBadgeIfNecessary);
+    if (!document || !props.botContainer) return
+    observer = new MutationObserver(appendBadgeIfNecessary)
     observer.observe(props.botContainer, {
       subtree: false,
       childList: true,
-    });
-  });
+    })
+  })
 
   onCleanup(() => {
-    if (observer) observer.disconnect();
-  });
+    if (observer) observer.disconnect()
+  })
 
   return (
     <span
@@ -81,26 +81,17 @@ export const Badge = (props: Props) => {
       {props.showButton && (
         <button
           onClick={() => {
-            window.open(
-              props.buttonLink, "_blank"
-            );
+            window.open(props.buttonLink, "_blank");
           }}
           style={{
-            border: `2px solid ${props.buttonColor || "#303235"}`,
+            border: `2px solid ${props.buttonColor || '#303235'}`,
             "border-radius": "5px",
             padding: "12.5px",
-            display: "flex",
-            "align-items": "center",
             cursor: "pointer",
           }}
         >
           <StarIcon />
-          <span
-            style={{
-              "margin-left": "5px",
-              color: props.buttonColor || "#303235",
-            }}
-          >
+          <span style={{ "margin-left": "5px", color: props.buttonColor || '#303235' }}>
             {props.buttonText}
           </span>
         </button>
