5ebe3f779c12ae455868d35746f867f3e4066b1e
Gabriel Lechenco <gabriel.lechenco@startse.com>
2024-02-19 08:43:48 -0300

    feat: add tooltip to source documents

---
+++ dist/components/Tooltip.d.ts
@@ -0,0 +1,8 @@
+import { JSXElement } from "solid-js";
+type Props = {
+    message: string;
+    children: JSXElement;
+};
+export declare const Tooltip: (props: Props) => import("solid-js").JSX.Element;
+export {};
+//# sourceMappingURL=Tooltip.d.ts.map
--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -9,6 +9,7 @@ import { BotMessageTheme, TextInputTheme, UserMessageTheme } from '@/features/bu
 import { Badge } from './Badge';
 import socketIOClient from 'socket.io-client';
 import { Popup } from '@/features/popup';
+import { Tooltip } from './Tooltip';
 
 type messageType = 'apiMessage' | 'userMessage' | 'usermessagewaiting';
 
@@ -53,24 +54,24 @@ const defaultWelcomeMessage = 'Hi there! How can I help?';
     },
     {
         "pageContent": "sistance,  and  polishing  [65].  For  instance,  AI  tools  generate\nsuggestions based on inputting keywords or topics. The tools\nanalyze  search  data,  trending  topics,  and  popular  queries  to\ncreate  fresh  content.  What’s  more,  AIGC  assists  in  writing\narticles and posting blogs on specific topics. While these tools\nmay not be able to produce high-quality content by themselves,\nthey can provide a starting point for a writer struggling with\nwriter’s block.\nH.  Cons of AIGC\nOne of the main concerns among the public is the potential\nlack  of  creativity  and  human  touch  in  AIGC.  In  addition,\nAIGC sometimes lacks a nuanced understanding of language\nand context, which may lead to inaccuracies and misinterpre-\ntations. There are also concerns about the ethics and legality\nof using AIGC, particularly when it results in issues such as\ncopyright  infringement  and  data  privacy.  In  this  section,  we\nwill discuss some of the disadvantages of AIGC (Table IV).",
-        "metadata": {
-          "source": "blob",
-          "blobType": "",
-          "pdf": {
-            "version": "1.10.100",
-            "info": {
-              "PDFFormatVersion": "1.5",
-              "IsAcroFormPresent": false,
-              "IsXFAPresent": false,
-              "Title": "",
-              "Author": "",
-              "Subject": "",
-              "Keywords": "",
-              "Creator": "LaTeX with hyperref",
-              "Producer": "pdfTeX-1.40.21",
-              "CreationDate": "D:20230414003603Z",
-              "ModDate": "D:20230414003603Z",
-              "Trapped": {
+    "metadata": {
+    "source": "blob",
+    "blobType": "",
+    "pdf": {
+    "version": "1.10.100",
+    "info": {
+    "PDFFormatVersion": "1.5",
+    "IsAcroFormPresent": false,
+    "IsXFAPresent": false,
+    "Title": "",
+    "Author": "",
+    "Subject": "",
+    "Keywords": "",
+    "Creator": "LaTeX with hyperref",
+    "Producer": "pdfTeX-1.40.21",
+    "CreationDate": "D:20230414003603Z",
+    "ModDate": "D:20230414003603Z",
+    "Trapped": {
                 "name": "False"
               }
             },
@@ -88,9 +89,9 @@ const defaultWelcomeMessage = 'Hi there! How can I help?';
     },
     {
         "pageContent": "Main article: Views of Elon Musk",
-        "metadata": {
-          "source": "https://en.wikipedia.org/wiki/Elon_Musk",
-          "loc": {
+    "metadata": {
+    "source": "https://en.wikipedia.org/wiki/Elon_Musk",
+    "loc": {
             "lines": {
               "from": 2409,
               "to": 2409
@@ -100,7 +101,7 @@ const defaultWelcomeMessage = 'Hi there! How can I help?';
     },
     {
         "pageContent": "First Name: John\nLast Name: Doe\nAddress: 120 jefferson st.\nStates: Riverside\nCode: NJ\nPostal: 8075",
-        "metadata": {
+    "metadata": {
           "source": "blob",
           "blobType": "",
           "line": 1,
@@ -129,7 +130,7 @@ export const Bot = (props: BotProps & { class?: string }) => {
       {
         message: props.welcomeMessage ?? defaultWelcomeMessage,
         type: 'apiMessage',
-      },
+              },
     ],
     { equals: false },
   );
@@ -402,12 +403,14 @@ export const Bot = (props: BotProps & { class?: string }) => {
                       <For each={[...removeDuplicateURL(message)]}>
                         {(src) => {
                           return (
-                            <a href={src.metadata.href} target='_blank'>
-                              <SourceBubble
-                                pageContent={src.metadata.titulo ? src.metadata.titulo : src.pageContent}
-                                metadata={src.metadata}
-                              />
-                            </a>
+                            <Tooltip message={src.pageContent}>
+                              <a href={src.metadata.href} target='_blank'>
+                                <SourceBubble
+                                  pageContent={src.metadata.titulo ? src.metadata.titulo : src.pageContent}
+                                  metadata={src.metadata}
+                                />
+                              </a>
+                            </Tooltip>
                           );
                         }}
                       </For>
