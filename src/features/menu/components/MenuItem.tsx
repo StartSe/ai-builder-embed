c165105c68a861907a3c3f21c02697298ea5578a
Gabriel Lechenco <gabriel.lechenco@startse.com>
2024-02-21 13:51:39 -0300

    feat: redirect to same folder level

--- src/features/menu/components/MenuItem.tsx
+++ src/features/menu/components/MenuItem.tsx
@@ -1,16 +1,18 @@
 export interface MenuItemProps {
-    id: string;
-    title: string;
-    subtitle: string;
-    selected?: boolean;
+  id: string;
+  title: string;
+  subtitle: string;
+  selected?: boolean;
 }
 
 export const MenuItem = (props: MenuItemProps) => {
-    return <a href={`/${props.id}.html`}>
-        <div class={`menu-item ${props.selected ? 'selected' : ''}`}>
-            <p>
-                <b>{props.title}</b> - {props.subtitle}
-            </p>
-        </div>
+  return (
+    <a href={`/${props.id}.html`}>
+      <div class={`menu-item ${props.selected ? 'selected' : ''}`}>
+        <p>
+          <b>{props.title}</b> - {props.subtitle}
+        </p>
+      </div>
     </a>
-}
+  );
+};
