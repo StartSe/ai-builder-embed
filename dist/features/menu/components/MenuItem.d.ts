590989c089d039007d6fed2d7d546c817abb370e
Samuel Jacobsen <samuel.jacobsen@startse.com>
2024-04-25 13:56:49 -0300

    feat: add avatar menu

--- dist/features/menu/components/MenuItem.d.ts
+++ dist/features/menu/components/MenuItem.d.ts
@@ -3,6 +3,7 @@ export interface MenuItemProps {
     title: string;
     subtitle: string;
     selected?: boolean;
+    avatarSrc: string;
 }
 export declare const MenuItem: (props: MenuItemProps) => import("solid-js").JSX.Element;
 //# sourceMappingURL=MenuItem.d.ts.map
