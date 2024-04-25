c4470320f312307ba594cbc58c4b7da594a6d3d8
Samuel Jacobsen <samuel.jacobsen@startse.com>
2024-04-25 13:56:49 -0300

    feat: add avatar menu

--- src/features/menu/components/MenuItem.tsx
+++ src/features/menu/components/MenuItem.tsx
@@ -1,14 +1,18 @@
+import { Avatar } from "@/components/avatars/Avatar";
+
 export interface MenuItemProps {
   id: string;
   title: string;
   subtitle: string;
   selected?: boolean;
+  avatarSrc: string;
 }
 
 export const MenuItem = (props: MenuItemProps) => {
   return (
     <a href={`/${props.id}.html`}>
       <div class={`menu-item ${props.selected ? 'selected' : ''}`}>
+        <Avatar initialAvatarSrc={props.avatarSrc} />
         <p>
           <b>{props.title}</b> - {props.subtitle}
         </p>
